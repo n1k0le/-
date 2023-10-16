38d2574ab5eb974c06a114468e89ab2abf8bc23b
n1k0le <137985393+n1k0le@users.noreply.github.com>
2023-10-16 11:42:29 +0300

    Add files via upload

---
+++ шифровальщик/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            char[] chars = textBox1.Text.ToCharArray();
+            foreach (char c in chars)
+                textBox2.Text += Convert.ToChar(c ^ 5);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            char[] chars = textBox2.Text.ToCharArray();
+            foreach (char c in chars)
+                textBox1.Text += Convert.ToChar(c ^ 5);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();textBox1.Clear();
+        }
+    }
+}
